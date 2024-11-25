284f7e2cfe4dd78477e2612599806e290ddedefc
viickywalker <vmw122@ic.ac.uk>
2024-11-25 22:30:18 +0000

    pre-test commit

---
+++ software_test_2024/src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {				// A class called Main
+    public static void main(String[] args){	// The program entry point
+        System.out.println("Hello, World!");	// Print to screen
+    }
+}
+
